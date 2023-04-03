1024a95da66570033b3d074d8727579695151a4d
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-04-03 16:15:11 +0200

    Change addEventListener repetitions to forEach

--- javascript.js
+++ javascript.js
@@ -44,7 +44,7 @@ function operatorIsClicked(clickedSymbol){
     if (operandNumber === 2){
         // Does nothing
         showOnDisplay();
-    } else if (clickedSymbol === "sqrt") {
+    } else if (clickedSymbol === "√") {
         operator = clickedSymbol;
         calculate();
     } else {
@@ -82,7 +82,7 @@ function dotIsClicked(clickedSymbol){
 }
 
 function calculate(){
-    if (secondOperand !== "" || operator === "sqrt"){
+    if (secondOperand !== "" || operator === "√"){
         let result = 0;
         if (percentageOnOperand !== 0) {
             if (operator === "*"){
@@ -96,7 +96,7 @@ function calculate(){
             }
         } else {
             switch (operator){
-                case ("sqrt"):
+                case ("√"):
                     result = Math.sqrt(+firstOperand);
                     break;
                 case ("^"):
@@ -146,117 +146,44 @@ function reset(result){
 }
 
 
-let display = document.querySelector(".display");
-let displayText = document.querySelector("#displayText");
+const displayText = document.querySelector("#displayText");
 displayText.textContent = "0";
 
-//[more efficient way of getting every button in a loop]
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((button) =>
+  button.addEventListener('click', function() {
+    clickedSymbol = button.textContent;
+    numberIsClicked(clickedSymbol);
+  }));
 
-let zero = document.querySelector("#zero");
-zero.addEventListener('click', function() {
-    clickedSymbol = "0";
-    numberIsClicked(clickedSymbol);
-  });
-let one = document.querySelector("#one");
-one.addEventListener('click', function() {
-    clickedSymbol = "1";
-    numberIsClicked(clickedSymbol);
-  });
-let two = document.querySelector("#two");
-two.addEventListener('click', function() {
-    clickedSymbol = "2";
-    numberIsClicked(clickedSymbol);
-  });
-let three = document.querySelector("#three");
-three.addEventListener('click', function() {
-    clickedSymbol = "3";
-    numberIsClicked(clickedSymbol);
-  });
-let four = document.querySelector("#four");
-four.addEventListener('click', function() {
-    clickedSymbol = "4";
-    numberIsClicked(clickedSymbol);
-  });
-let five = document.querySelector("#five");
-five.addEventListener('click', function() {
-    clickedSymbol = "5";
-    numberIsClicked(clickedSymbol);
-  });
-let six = document.querySelector("#six");
-six.addEventListener('click', function() {
-    clickedSymbol = "6";
-    numberIsClicked(clickedSymbol);
-  });
-let seven = document.querySelector("#seven");
-seven.addEventListener('click', function() {
-    clickedSymbol = "7";
-    numberIsClicked(clickedSymbol);
-  });
-let eight = document.querySelector("#eight");
-eight.addEventListener('click', function() {
-    clickedSymbol = "8";
-    numberIsClicked(clickedSymbol);
-  });
-let nine = document.querySelector("#nine");
-nine.addEventListener('click', function() {
-    clickedSymbol = "9";
-    numberIsClicked(clickedSymbol);
-  });
+  const operatorButtons = document.querySelectorAll(".operator");
+  operatorButtons.forEach((button) =>
+  button.addEventListener('click', function() {
+    clickedSymbol = button.textContent;
+    operatorIsClicked(clickedSymbol);
+  }));
 
 
-let percentageButton = document.querySelector("#percent");
+//-------------------
+
+const percentageButton = document.querySelector("#percent");
 percentageButton.addEventListener('click', function() {
     clickedSymbol = "%";
     percentageIsClicked(clickedSymbol);
 })
 
-let dotButton = document.querySelector("#dot");
+const dotButton = document.querySelector("#dot");
 dotButton.addEventListener('click', function() {
     clickedSymbol = ".";
     dotIsClicked(clickedSymbol);
 })
 
+ //-------------------------
 
-let sqrtButton = document.querySelector("#sqrt");
-sqrtButton.addEventListener('click', function() {
-    clickedSymbol = "sqrt";
-    operatorIsClicked(clickedSymbol);
-})
-
-let expButton = document.querySelector("#exp");
-expButton.addEventListener('click', function() {
-    clickedSymbol = "^";
-    operatorIsClicked(clickedSymbol);
-})
-
-let multiplyButton = document.querySelector("#multiply");
-multiplyButton.addEventListener('click', function() {
-    clickedSymbol = "*";
-    operatorIsClicked(clickedSymbol);
-})
-
-let divideButton = document.querySelector("#divide");
-divideButton.addEventListener('click', function() {
-    clickedSymbol = "/";
-    operatorIsClicked(clickedSymbol);
-})
-
-let subtractButton = document.querySelector("#subtract");
-subtractButton.addEventListener('click', function() {
-    clickedSymbol = "-";
-    operatorIsClicked(clickedSymbol);
-})
-
- let addButton = document.querySelector("#add");
- addButton.addEventListener('click', function() {
-     clickedSymbol = "+";
-     operatorIsClicked(clickedSymbol);
- })
-
- let clearButton = document.querySelector("#c");
+ const clearButton = document.querySelector("#c");
  clearButton.addEventListener("click", function() {
         location.reload(true);  // Refreshes the page when clear button is clicked
     });
 
-let resultButton = document.querySelector("#equals");
+const resultButton = document.querySelector("#equals");
 resultButton.addEventListener("click", calculate);
