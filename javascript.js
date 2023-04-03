6c9b5ff3f52f8a8a16dd2b6fa59374eaf011cbb3
CCartepillarr <127199181+CCaterpillarr@users.noreply.github.com>
2023-04-03 04:42:07 +0200

    Add skeleton of cases for clicked operands

--- javascript.js
+++ javascript.js
@@ -1,32 +1,49 @@
 let operandNumber = 1;
-let operandSymbolNumber = 1;
+let symbolNumber = 0;
 let firstOperand = "0";
 let operator = "";
 let secondOperand = "";
-let clickedNumber = "";
+let clickedSymbol = "";
 
 function showOnDisplay(){
     displayText.textContent= firstOperand + operator + secondOperand;
 }
 
-// 0123456789 clicked:
-function numberIsClicked(clickedNumber){
-    // IF on first operand AND on first symbol:
-    if (operandNumber === 1 && operandSymbolNumber === 1){
-        firstOperand = clickedNumber;
+function numberIsClicked(clickedSymbol){
+    if (operandNumber === 1 && symbolNumber === 0){
+        firstOperand = clickedSymbol; // Replace current symbol with the number clicked (as a string)
         showOnDisplay();
-        operandSymbolNumber++;
-    } else {
-        firstOperand = firstOperand + clickedNumber;
+        symbolNumber++;
+    } else {  
+        firstOperand = firstOperand + clickedSymbol; // Add the number as a string to the current operator
         showOnDisplay();
-        operandSymbolNumber++;
+        symbolNumber++;
     }
 } 
 
+function operatorIsClicked(clickedSymbol){
+    if (operandNumber === 2){
+        // Does nothing
+    } else {
+        switch (clickedSymbol){
+            case ("sqrt"):
+                break;
+            case ("^"):
+                break;
+            case ("x"):
+                break;
+            case ("/"):
+                break;
+            case ("-"):
+                break;
+            case ("+"):
+                break;
+            default:
+                break;
+        }
+    }
 
-//  -replace current symbol with the number clicked (as a string)
-// ELSE:
-//  -add the number as a string to the current operator
+}
 
 // operator clicked:
 // IF on second operand:
@@ -72,52 +89,71 @@ displayText.textContent = "0";
 //[more efficient way of getting every button in a loop]
 
 let zero = document.querySelector("#zero");
-zero.addEventListener('click', function(number) {
-    clickedNumber = "0";
-    numberIsClicked(clickedNumber);
+zero.addEventListener('click', function() {
+    clickedSymbol = "0";
+    numberIsClicked(clickedSymbol);
   });
 let one = document.querySelector("#one");
-one.addEventListener('click', function(number) {
-    clickedNumber = "1";
-    numberIsClicked(clickedNumber);
+one.addEventListener('click', function() {
+    clickedSymbol = "1";
+    numberIsClicked(clickedSymbol);
   });
 let two = document.querySelector("#two");
-two.addEventListener('click', function(number) {
-    clickedNumber = "2";
-    numberIsClicked(clickedNumber);
+two.addEventListener('click', function() {
+    clickedSymbol = "2";
+    numberIsClicked(clickedSymbol);
   });
 let three = document.querySelector("#three");
-three.addEventListener('click', function(number) {
-    clickedNumber = "3";
-    numberIsClicked(clickedNumber);
+three.addEventListener('click', function() {
+    clickedSymbol = "3";
+    numberIsClicked(clickedSymbol);
   });
 let four = document.querySelector("#four");
-four.addEventListener('click', function(number) {
-    clickedNumber = "4";
-    numberIsClicked(clickedNumber);
+four.addEventListener('click', function() {
+    clickedSymbol = "4";
+    numberIsClicked(clickedSymbol);
   });
 let five = document.querySelector("#five");
-five.addEventListener('click', function(number) {
-    clickedNumber = "5";
-    numberIsClicked(clickedNumber);
+five.addEventListener('click', function() {
+    clickedSymbol = "5";
+    numberIsClicked(clickedSymbol);
   });
 let six = document.querySelector("#six");
-six.addEventListener('click', function(number) {
-    clickedNumber = "6";
-    numberIsClicked(clickedNumber);
+six.addEventListener('click', function() {
+    clickedSymbol = "6";
+    numberIsClicked(clickedSymbol);
   });
 let seven = document.querySelector("#seven");
-seven.addEventListener('click', function(number) {
-    clickedNumber = "7";
-    numberIsClicked(clickedNumber);
+seven.addEventListener('click', function() {
+    clickedSymbol = "7";
+    numberIsClicked(clickedSymbol);
   });
 let eight = document.querySelector("#eight");
-eight.addEventListener('click', function(number) {
-    clickedNumber = "8";
-    numberIsClicked(clickedNumber);
+eight.addEventListener('click', function() {
+    clickedSymbol = "8";
+    numberIsClicked(clickedSymbol);
   });
 let nine = document.querySelector("#nine");
-nine.addEventListener('click', function(number) {
-    clickedNumber = "9";
-    numberIsClicked(clickedNumber);
+nine.addEventListener('click', function() {
+    clickedSymbol = "9";
+    numberIsClicked(clickedSymbol);
   });
+
+
+let percentageButton = document.querySelector("#percent");
+percentageButton.addEventListener('click', function() {
+    clickedSymbol = "%";
+    operatorIsClicked(clickedSymbol);
+})
+
+// let percentageButton = document.querySelector("#percent");
+// percentageButton.addEventListener('click', function() {
+//     clickedSymbol = "%";
+//     percentageIsClicked(clickedSymbol);
+// })
+
+ let addButton = document.querySelector("#add");
+ addButton.addEventListener('click', function() {
+     clickedSymbol = "+";
+     operatorIsClicked(clickedSymbol);
+ })
